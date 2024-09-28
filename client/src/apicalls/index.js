4a067ca10fa1326ccd78803e4c5e7028e9cd8fc0
SupriyaSudini <supriyasudini165@gmail.com>
2024-09-28 15:22:44 -0400

    updated with notification

--- client/src/apicalls/index.js
+++ client/src/apicalls/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Replace 'https://your-deployment-link' with your actual deployment link
 export const axiosInstance = axios.create({
-    // baseURL: 'https://pvs-wallet-backend.vercel.app/',
+    baseURL: 'https://pvs-wallet-backend.vercel.app/',
     headers: {
         'authorization': `Bearer ${localStorage.getItem('token')}`
     }
